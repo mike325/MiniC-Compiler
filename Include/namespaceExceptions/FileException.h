e0e2bfe45b6abaa3b917b189aa4f950c28c4b60d
Mike325 <miguel.8a.hdez@gmail.com>
2015-12-16 14:15:50 -0600

    added FileException class

---
+++ Include/namespaceExceptions/FileException.h
@@ -0,0 +1,25 @@
+//
+// Created by mike on 16/12/15.
+//
+
+#ifndef FILEEXCEPTION_H
+#define FILEEXCEPTION_H
+
+#include "../../Include/namespaceExceptions/GeneralException.h"
+#include <iostream>
+
+namespace exceptions
+{
+    class FileException : public GeneralException
+    {
+    public:
+
+        FileException ( std::string message, std::string description );
+        ~FileException () throw();
+        const char* what() const throw();
+
+    private:
+    };
+}
+
+#endif //COMPILER_FILEEXCEPTION_H
---
+++ src/namespaceExceptions/FileException.cpp
@@ -0,0 +1,25 @@
+//
+// Created by mike on 16/12/15.
+//
+
+#include <string>
+#include "../../Include/namespaceExceptions/FileException.h"
+
+exceptions::FileException::FileException(std::string message, std::string description) : GeneralException()
+{
+    this->message.assign(message);
+    this->message.append(" could not been loaded");
+    this->description.assign("The file ");
+    this->description.append(this->message);
+}
+
+exceptions::FileException::~FileException () throw()
+{
+    this->message = "";
+    this->description = "";
+}
+
+const char* exceptions::FileException::what() const throw()
+{
+    return  this->description.c_str();
+}
