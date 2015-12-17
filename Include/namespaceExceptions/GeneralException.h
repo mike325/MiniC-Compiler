1594e7849be925835e6fe1291abc4cc0aa2d840f
Mike325 <miguel.8a.hdez@gmail.com>
2015-12-17 01:36:25 -0600

    code format changed

--- Include/namespaceExceptions/GeneralException.h
+++ Include/namespaceExceptions/GeneralException.h
@@ -6,24 +6,34 @@
 #define EXCEPTIONS_H
 
 #include <iostream>
+#include <stdexcept>
 
 namespace exceptions
 {
     class GeneralException : public std::exception
     {
-    public:
+      public:
+        GeneralException()
+            : exception()
+            , message( "" )
+            , description( "Has been an usxpected error " )
+        {
+        }
+        GeneralException( std::string message, std::string description )
+            : exception()
+            , message( message )
+            , description( description )
+        {
+        }
+        virtual ~GeneralException() throw(){};
+        virtual const char *what() const throw() { return this->description.c_str(); }
 
-        GeneralException ( ) : exception( ), message(""), description("Has been an usxpected error ") {}
-        GeneralException ( std::string message, std::string description ) : exception( ), message( message ), description(description) {}
-        virtual ~GeneralException () throw() {};
-        virtual const char* what() const throw() { return this->description.c_str(); }
-
-    protected:
+      protected:
         std::string message;
         std::string description;
 
-    private:
+      private:
     };
 }
 
-#endif  // EXCEPTIONS_H
+#endif    // EXCEPTIONS_H
--- src/namespaceExceptions/FileException.cpp
+++ src/namespaceExceptions/FileException.cpp
@@ -5,12 +5,13 @@
 #include <string>
 #include "../../Include/namespaceExceptions/FileException.h"
 
-exceptions::FileException::FileException( std::string message ) : GeneralException()
+exceptions::FileException::FileException( std::string message )
+    : GeneralException()
 {
-    this->message.assign( message );
-    this->message.append( " could not be loaded" );
     this->description.assign( "The file " );
+    this->message.assign( message );
+    this->message.append( " could not be load" );
     this->description.append( this->message );
 }
 
-const char* exceptions::FileException::what() const throw() { return this->description.c_str(); }
+const char *exceptions::FileException::what() const throw() { return this->description.c_str(); }
